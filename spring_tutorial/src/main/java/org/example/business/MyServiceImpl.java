293f8eef076acc36886b2abd10d7f2b21296e814
rala8730 <rasmi@rasmis-MacBook-Air.local>
2023-01-02 21:02:05 -0700

    Adding qualifierto manage interface

--- spring_tutorial/src/main/java/org/example/business/MyServiceImpl.java
+++ spring_tutorial/src/main/java/org/example/business/MyServiceImpl.java
@@ -4,6 +4,7 @@ package org.example.business;
 import org.example.data.MyRepository;
 import org.example.data.SomeOtherRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 //@Component
 @Service //using service instead of component //removing this to create java based bean in AppConfig
@@ -14,8 +15,8 @@ public class MyServiceImpl implements MyService {
     //loosely coupled constructor
     // constructor dependency type
 @Autowired //removing this to create java based bean in AppConfig
-    public MyServiceImpl(SomeOtherRepositoryImpl repository){
-    
+    //public MyServiceImpl(SomeOtherRepositoryImpl repository){
+    public MyServiceImpl(@Qualifier("myRepositoryImpl") MyRepository repository){ // adding qualifier
         this.repository=repository;
     }
 
